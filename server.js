8b894a8e2cdf01a609dfa591aab90f984a1918e2
NileshDeshmukh09 <nilesdeshmukh0908@gmail.com>
2022-09-02 23:41:22 +0530

    setup PORT in Dotenv

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express =require("express");
-const PORT = 8888
+const TicketServerConfig = require("./configs/TicketServer.config");
+
 
 const app = express();
 
 
-app.listen(PORT, () => {
-    console.log(`Ticket-Creation-Module-Server has started on the port http://localhost:${PORT}` );
+app.listen(TicketServerConfig.PORT, () => {
+    console.log(`Ticket-Creation-Module-Server has started on the port http://localhost:${TicketServerConfig.PORT}` );
 })
