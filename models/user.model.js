dd648be7e59aa3e917c5cca8b8593e176880b67a
NileshDeshmukh09 <nilesdeshmukh0908@gmail.com>
2022-09-02 23:50:50 +0530

    Added user Model schema

---
+++ models/user.model.js
@@ -0,0 +1,63 @@
+
+ /**
+ * Schema for the user Model will be provided Here
+ */
+
+  const mongoose = require("mongoose");
+  const databaseConfig = require("../configs/database.config");
+  
+  const userSchema = new mongoose.Schema({
+  
+      name : {
+          type : String,
+          required : true
+      },
+      userId : {
+          type : String,
+          required : true,
+          unique : true
+      },
+      password :{
+          type : String,
+          required : true
+      },
+      email : {
+          type : String,
+          required : true,
+          lowercase : true,
+          minLength : 10,
+          unqiue : true
+      }, 
+      createdAt : {
+          type : Date,
+          immutable : true,
+          default : ()=>{
+              return Date.now();
+          }
+      },
+      updatedAt : {
+          type : Date,
+          default : ()=>{
+              return Date.now();
+          }
+      },
+      userType : {
+          type : String,
+          required : true,
+          default : "CUSTOMER"
+      },
+      userStatus : {
+          type : String,
+          required : true,
+          default : "APPROVED"
+      },
+    
+  
+  });
+  
+  
+  
+  module.exports = mongoose.model("User", userSchema);
+  
+  
+  
