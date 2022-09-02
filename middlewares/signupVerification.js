b30aa9c1a6b2beb412500a07a60383ec826e31da
NileshDeshmukh09 <nilesdeshmukh0908@gmail.com>
2022-09-03 00:32:01 +0530

    password Validation midddlware

--- middlewares/signupVerification.js
+++ middlewares/signupVerification.js
@@ -60,7 +60,18 @@
         return res.status(400).send("UserType is Not Correct !")
     }
 
-  
+    /** validate the "password" if it Exists */  
+    if( !req.body.password ){
+        return res.status(400).send("Password is not provided")
+    }
+    
+    /** validate the Password */
+    const passwordPattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/;
+
+    // message : "Password must have at least 1 upper case, 1 lower case, 1 digit, 1 special characters, and should be 8 characters in length."
+    if( !passwordPattern.test( req.body.password )){
+        return res.status(400).send( `Password must have at least - 1 upper case, 1 lower case,  1 digit, 1 special characters,  and should be 8 characters in length.`)
+    }
     
      next(); // give the controll to the controller
  }
