4b0ec8b096dcff89a975489ae09df8195d7bfa81
NileshDeshmukh09 <nilesdeshmukh0908@gmail.com>
2022-09-03 00:48:32 +0530

    ROutes for Login

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -12,5 +12,8 @@ const router = express.Router();
 /** SIGNUP - POST */
 router.post("/ticketsmodule/api/v1/auth/signup",[signupVerification.addMiddlewaresToSignupRequest], authController.signup);
 
+ /** SIGNIN - POST */
+router.post("/ticketsmodule/api/v1/auth/signin", authController.signin);
+
 module.exports = router
    
