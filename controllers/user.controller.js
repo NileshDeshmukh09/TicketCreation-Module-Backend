473c0c4f99fe559ba95e50c61f57e71104d067bb
NileshDeshmukh09 <nilesdeshmukh0908@gmail.com>
2022-09-03 07:08:10 +0530

    Controller for find userIdByID

---
+++ controllers/user.controller.js
@@ -0,0 +1,36 @@
+/**
+ * This file will have all the logic to manipulate the User resource 
+*/
+
+const User = require("../models/user.model");
+
+
+/**
+ * Fetch the User based on the UserID
+ */
+ async function findUserByID(req, res){
+    const userIDRequest = req.params.userId; // Request Paramater Reads
+
+    const user = await  User.findOne({ userId : userIDRequest });
+
+    if( user ){
+        return res.status(200).send({
+            status : 200,
+            message : "Successfully Fetched  user !",  
+            
+        });
+    }else{
+        return res.status(200).send({
+            status : 200,
+            message : "user with ID :  "+ userIDRequest + " - doesn't exist",
+        })
+    }
+
+}
+
+/** 
+ * Update the User - Status , userType
+ *   - only ADMIN shouldd be allowed to do this !
+ * 
+ * ADMIN  - name , userStatus , userType
+ */
