436d33b3252de5672b32a9215058eb3cf8b7696b
NileshDeshmukh09 <nilesdeshmukh0908@gmail.com>
2022-09-03 09:28:36 +0530

    create routes for all tickets

--- routes/ticket.routes.js
+++ routes/ticket.routes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const ticketController = require("../controllers/ticket.controller");
-const { authJWT, JWTAuth } = require("../middlewares");
+const {  JWTAuth } = require("../middlewares");
 
 module.exports = router
 
-    /** createTicket - POST  */
+/** createTicket - POST  */
 router.post("/ticketsmodule/api/v1/tickets" ,[ JWTAuth.verifyToken ], ticketController.createTicket );
+
+ /**GET ALL TICKETS - GET */
+router.get("/ticketsmodule/api/v1/tickets" ,[ JWTAuth.verifyToken ], ticketController.getAllTickets );
     
   
