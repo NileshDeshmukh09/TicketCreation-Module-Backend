32ee80fc409774dd060f6b603bfb2ea18af68311
NileshDeshmukh09 <nilesdeshmukh0908@gmail.com>
2022-09-03 07:20:29 +0530

    controller for Find all users

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,6 +5,34 @@
 const User = require("../models/user.model");
 const responseConvertor = require("../utils/responseConvertor")
 
+/**Get all the Users */
+async function findAllUsers(req, res){
+
+    try {
+        const users  = await User.find();
+
+        if( users.length > 0 ){
+            return res.status(200).send({
+                status : 200,
+                message : "Successfully Fetched All users !",
+                users : objectConvertor.userResponse(users)// user Password will not be Returned in response.
+            });
+        }else{
+            return res.status(200).send({
+                status : 200,
+                message : "No user found !"
+                // users : objectConvertor.userResponse(users)// user Password will not be Returned in response.
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        return  res.status(500).send({
+            status: 500,
+            message : "Internal Error while Fetching all Users."
+        })
+    }
+}
+
 
 /**
  * Fetch the User based on the UserID
