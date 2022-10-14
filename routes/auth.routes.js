4a47688f4e86d55e959a622eb60be65c4af12592
NileshDeshmukh09 <nileshdeshmukh0908@gmail.com>
2022-10-14 17:52:48 +0530

    Addede comments for user and passwords

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,18 +2,20 @@
  * Routes for Authentication and Authorization
  */
 
-// RESTFULL -APIs for Authentication
-const express = require("express");
-const authController = require("../controllers/auth.controller");
-const { signupVerification } = require("../middlewares");
-const router = express.Router();
-
 // UserType  |  UserID | Passsword 
 
 // Customer  |  Nil11 | newNil11@
 // Engineer  |  Nil02 | Customer@123
 // Engineer  | user02  | newUser02@
 // ADMIN     | admin |  Welcome987
+
+// RESTFULL -APIs for Authentication
+const express = require("express");
+const authController = require("../controllers/auth.controller");
+const { signupVerification } = require("../middlewares");
+const router = express.Router();
+
+
 
 /** SIGNUP - POST */
 router.post("/auth/signup", [signupVerification.addMiddlewaresToSignupRequest] , authController.signup);
